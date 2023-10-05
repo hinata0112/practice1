e00298a1a4cf0380704d04d292348b54c8cf4518
h.motodate <h.motodate.sys23@morijyobi.ac.jp>
2023-10-05 10:37:59 +0900

    pra2.javaにさらに追記
    １００回出力されます。

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
 pubiric class practice{
 	pubric static void main (String[] args){
-		for (int cnt = 0; cnt <= 100; cnt++)
+		for (int cnt = 1; cnt <= 100; cnt++)
 		System.out.println ("Hello World");
 	}
 }
